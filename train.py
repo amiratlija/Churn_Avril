edcc2442ff0822976456c465d56b356b90fa17b9
amiratlija <tlija.amira@gmail.com>
2026-04-09 17:16:25 +0200

    Import metrics for model evaluation
    Added import statements for additional metrics.

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import joblib
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 # Load the data, assuming the file 'customer_churn.csv' is available
 data=pd.read_csv('data/customer_churn.csv',sep=',')
